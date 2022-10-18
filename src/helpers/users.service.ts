b3e21f03e3a8ee3249becdd53d30067ffdabbb61
HosamAlden <hosam98j@gmail.com>
2022-10-18 22:53:01 +0200

    edit users services

--- src/helpers/users.service.ts
+++ src/helpers/users.service.ts
@@ -5,17 +5,8 @@ export function validate_user_updated_data(data: Partial<User>){
     if(data.email !== undefined) {
         updated_data.email = data.email
     }
-    if(data.username !== undefined) {
-        updated_data.username = data.username
-    }
-    if(data.phone !== undefined) {
-        updated_data.phone = data.phone
-    }
-    if(data.gender !== undefined) {
-        updated_data.gender = data.gender
-    }
-    if(data.phone !== undefined) {
-        updated_data.birthday = data.birthday
+    if(data.user_name !== undefined) {
+        updated_data.user_name = data.user_name
     }
     return updated_data
 }
